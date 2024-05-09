9bcbe9bd380bd62f3b89181846c4e630afeade51
abnjain <abnjain25@gmail.com>
2024-05-10 01:46:19 +0530

    trying to set header css file

--- Components/Header.js
+++ Components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+// import styles from "./Header.module.css";
 
 const Header = () => {
   return (
@@ -7,10 +8,11 @@ const Header = () => {
       <div className="mt-8 h-8 p-5 px-40 flex items-center justify-between text-2xl">
         <Link href="/"><h1>Logo</h1></Link>
         <div className="flex gap-8">
-          <Link href="/">home</Link>
-          <Link href="/About">about</Link>
-          <Link href="/Projects">projects</Link>
-          <Link href="/Blogs">blog</Link>
+        {/* <div className={`${styles.bli} flex gap-8`}>  // To apply css for the same page only */}
+          <Link href="/"><h3>home</h3></Link>
+          <Link href="/About"><h3>about</h3></Link>
+          <Link href="/Projects"><h3>projects</h3></Link>
+          <Link href="/Blogs"><h3>blog</h3></Link>
         </div>
       </div>
     </>
