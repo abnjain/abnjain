2dad7518903f081404e330053613f4b731e6d499
abnjain <abnjain25@gmail.com>
2024-05-09 19:25:05 +0530

    update

---
+++ Pages/_error.js
@@ -0,0 +1,13 @@
+// pages/_error.js
+
+const ErrorPage = () => {
+    return (
+      <div>
+        {/* Your custom error message or layout */}
+        <h1>Custom Error Page</h1>
+        <p>Oops! Something went wrong.</p>
+      </div>
+    );
+  };
+  
+export default ErrorPage;
