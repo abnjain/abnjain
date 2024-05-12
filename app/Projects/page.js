0c004fa6c2953c1f34bfd6e919a78eaf07452af0
abnjain <abnjain25@gmail.com>
2024-05-12 17:14:47 +0530

    update effects on project page and add some porjects

--- app/Projects/page.js
+++ app/Projects/page.js
@@ -6,10 +6,31 @@ const Projects = () => {
   return (
     <main className="flex min-h-max justify-center py-12">
       <div className="justify-center items-center">
-        <h1 className="text-4xl text-center font-bold">Projects Page</h1>
+        <h1 className="text-4xl text-center font-bold">Some Projects I Developed and Designed</h1>
         {/* <PortFolio /> */}
-        <div className="flex flex-wrap gap-16 mt-10 px-4 py-10 justify-center border-2 shadow-2xl shadow-neutral-50 rounded-xl max-w-7xl overflow-hidden">
-          <div className="relative w-72 h-96 rounded-3xl flex flex-col justify-center group">
+        <div className="flex flex-wrap gap-16 mt-10 px-4 py-10 justify-center border-2 shadow-2xl shadow-neutral-50 rounded-xl max-w-7xl h-fit overflow-hidden no-scrollbar">
+          {/* KUBER GROUP */}
+          <div className="relative w-72 h-96 rounded-3xl group">
+            <Image
+              src="/Images/kuberGroup.png" // Path to your image file in the public directory
+              alt="Stock Market Website"
+              className="object-cover w-full h-full rounded-3xl"
+              width={500} // Width of the image
+              height={300} // Height of the image
+            />
+            <a
+              href="https://kubergroup.in"
+              target="_blank"
+              className="absolute inset-0 flex justify-center items-center bg-transparent group-hover:backdrop-blur"
+              title="Visit Example Website"
+            >
+              <span className="text-xl px-3 rounded-3xl group-hover:px-16">
+                KUBER GROUP
+              </span>
+            </a>
+          </div>
+          {/* find-the-bubble GAME */}
+          <div className="relative w-72 h-96 rounded-3xl justify-center group">
             <Image
               src="/Images/findTheBubble.png" // Path to your image file in the public directory
               alt="find-the-bubble GAME"
@@ -20,15 +41,56 @@ const Projects = () => {
             <a
               href=" https://findthebubble.netlify.app/"
               target="_blank"
-              className="absolute inset-0 flex justify-center items-center bg-transparent"
+              className="absolute inset-0 flex justify-center items-center bg-transparent group-hover:backdrop-blur"
               title="Visit Example Website"
             >
-              <span className="inline-block px-24 text-xl rounded-3xl group-hover:bg-red-700">
+              <span className="text-xl px-3 rounded-3xl group-hover:px-10">
               find-the-bubble GAME
               </span>
             </a>
           </div>
-          <div className="relative w-72 h-96 rounded-3xl flex flex-col justify-center group">
+          {/* PORTFOLIO */}
+          <div className="relative w-72 h-96 rounded-3xl justify-center group">
+            <Image
+              src="/Images/portfolio.png" // Path to your image file in the public directory
+              alt="PORTFOLIO Website"
+              className="object-cover w-full h-full rounded-3xl"
+              width={500} // Width of the image
+              height={300} // Height of the image
+            />
+            <a
+              href="/"
+              // target="_blank"
+              className="absolute inset-0 flex justify-center items-center bg-transparent group-hover:backdrop-blur"
+              title="Visit Example Website"
+            >
+              <span className="text-xl px-3 rounded-3xl group-hover:px-24">
+                PORTFOLIO
+              </span>
+            </a>
+          </div>
+          {/* SHIKSHAK RECRUITMENT */}
+          <div className="relative w-72 h-96 rounded-3xl justify-center group">
+            <Image
+              src="/Images/shikshakRecruitment.png" // Path to your image file in the public directory
+              alt="Shikshak Recruitment Website"
+              className="object-cover w-full h-full rounded-3xl"
+              width={500} // Width of the image
+              height={300} // Height of the image
+            />
+            <a
+              href="/Projects"
+              // target="_blank"
+              className="absolute inset-0 flex justify-center items-center bg-transparent group-hover:backdrop-blur"
+              title="Visit Example Website"
+            >
+              <span className="text-xl px-3 rounded-3xl group-hover:px-7">
+                SHIKSHAK RECRUITMENT
+              </span>
+            </a>
+          </div>
+          {/* NOTEPAD */}
+          <div className="relative w-72 h-96 rounded-3xl justify-center group">
             <Image
               src="/Images/notepad.png" // Path to your image file in the public directory
               alt="NOTEPAD Website"
@@ -39,34 +101,16 @@ const Projects = () => {
             <a
               href="https://github.com/ABNjain/notepad"
               target="_blank"
-              className="absolute inset-0 flex justify-center items-center bg-transparent"
+              className="absolute inset-0 flex justify-center items-center bg-transparent group-hover:backdrop-blur"
               title="Visit Example Website"
             >
-              <span className="inline-block px-24 text-xl rounded-3xl group-hover:bg-red-700">
+              <span className="text-xl px-3 rounded-3xl group-hover:px-24">
                 NOTEPAD
               </span>
             </a>
           </div>
-          <div className="relative w-72 h-96 rounded-3xl flex flex-col justify-center group">
-            <Image
-              src="/Images/portfolio.png" // Path to your image file in the public directory
-              alt="PORTFOLIO Website"
-              className="object-cover w-full h-full rounded-3xl"
-              width={500} // Width of the image
-              height={300} // Height of the image
-            />
-            <a
-              href="/"
-              target="_blank"
-              className="absolute inset-0 flex justify-center items-center bg-transparent"
-              title="Visit Example Website"
-            >
-              <span className="inline-block px-24 text-xl rounded-3xl group-hover:bg-red-700">
-                PORTFOLIO
-              </span>
-            </a>
-          </div>
-          <div className="relative w-72 h-96 rounded-3xl flex flex-col justify-center group">
+          {/* GREYT HR Portal */}
+          <div className="relative w-72 h-96 rounded-3xl justify-center group">
             <Image
               src="/Images/" // Path to your image file in the public directory
               alt="GREYT HR Portal"
@@ -77,30 +121,11 @@ const Projects = () => {
             <a
               href="https://github.com/ABNjain/greytHR"
               target="_blank"
-              className="absolute inset-0 flex justify-center items-center bg-transparent"
+              className="absolute inset-0 flex justify-center items-center bg-transparent group-hover:backdrop-blur"
               title="Visit Example Website"
             >
-              <span className="inline-block px-24 text-xl rounded-3xl group-hover:bg-red-700">
+              <span className="text-xl px-3 rounded-3xl group-hover:px-16">
                 GREYT HR Portal
-              </span>
-            </a>
-          </div>
-          <div className="relative w-72 h-96 rounded-3xl flex flex-col justify-center group">
-            <Image
-              src="/Images/" // Path to your image file in the public directory
-              alt="Website"
-              className="object-cover w-full h-full rounded-3xl"
-              width={500} // Width of the image
-              height={300} // Height of the image
-            />
-            <a
-              href="#"
-              target="_blank"
-              className="absolute inset-0 flex justify-center items-center bg-transparent"
-              title="Visit Example Website"
-            >
-              <span className="inline-block px-24 text-xl rounded-3xl group-hover:bg-red-700">
-                
               </span>
             </a>
           </div>
