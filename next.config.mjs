cefa59da6f5bd659833959946e4f7a163bbaa44a
Abhi J <abnjain25@gmail.com>
2026-07-10 12:41:55 +0530

    fixed the duplicate next image crawling

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,7 @@ const nextConfig = {
         source: "/(.*)",
         headers: [
           { key: "X-Content-Type-Options", value: "nosniff" },
+          { key: "X-Robots-Tag", value: "noindex" },
           { key: "X-Frame-Options", value: "SAMEORIGIN" },
           { key: "X-XSS-Protection", value: "1; mode=block" },
           { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
